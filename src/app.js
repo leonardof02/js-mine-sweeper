e52a3ce52f33b747bea76c0f6fc254c0121de727
_leodev <leonardoarencibia2002@gmail.com>
2022-08-01 03:41:45 -0400

    Game Logic Finished, some fixes with UI

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+export class Block {
+    constructor(){
+        this.state = false;
+        this.label = 0;
+        this.flag = false;
+    }
+}
+
+export class Mine {
+    constructor() {
+        this.state = false;
+        this.label = 'm';
+        this.flag = false;
+    }
+}
+
 export class Grid {
     constructor( row, col ) {
         this.grid;
@@ -8,62 +24,52 @@ export class Grid {
 
     // Build a void matrix for introducing data
     buildEmptyGrid() {
-        this.grid = new Array( this.row );
-        for( let i = 0; i < this.row; i++ )
-            this.grid[i] = new Array( this.col ).fill(0, 0);
+        // NOTE: The matrix must have a row and col more than contains for correct processing
+        // the game logic and for preventing errors
+        this.grid = new Array();
+        for( let i = 0; i < this.row + 2; i++ ) {
+            const row = [];
+            for( let j = 0; j < this.col + 2; j++ ) {
+                row.push( new Block() );
+            }
+            this.grid[i] = [ ...row ];
+        }
     }
-
-    // Add a 4 new bouders for preventing Segmentation Fault
-    addLimiters() {
-        const emptyArr = new Array(this.col).fill(0);
-        this.grid.push( [...emptyArr] );
-        this.grid.unshift( [...emptyArr] );
-
-        this.grid.forEach( (value) => {
-            value.push(0);
-            value.unshift(0);
-        })
-    }
-
-    // Remove the bouders 
-    removeLimiters() {
-        const emptyArr = new Array(this.col).fill(0);
-        this.grid.pop();
-        this.grid.shift();
-
-        this.grid.forEach( (value) => {
-            value.pop();
-            value.shift();
-        })
-    }
-
     // Clear the grid and set to void values
     clearGrid() {
-        this.grid.forEach( ( value ) => {
-            value.fill(0);
-        })
+        this.grid.forEach( value => value.fill( new Block() ) );
     }
 
     // Show grid in a table
-    showGrid() {
+    showGameState() {
+        const gameState = this.grid.slice(1, this.row + 1).map( value => value.slice(1, this.col + 1));
+        console.table( gameState );
+    }
+
+    showOriginalGrid() {
         console.table( this.grid );
     }
 }
+
 export class MineGrid extends Grid {
     constructor( row, col ) {
         super( row, col );
         this.mines;
+        this.countR = 0;
     }
 
     // Count all the mines in the grid
     countMines() {
         let mines = 0;
-        for( let i in this.grid ) {
-            this.grid[i].forEach( ( value ) => {
-                if( value === 'm' )
+
+        for( let i = 1; i <= this.row; i++ ) {
+            for( let j = 1; j <= this.col; j++ ) {
+                if( ( this.grid[i][j] instanceof Mine ) ) {
                     mines++;
-            })
+                }
+            }
         }
+
         this.mines = mines;
         return this.mines;
     }
@@ -71,9 +77,9 @@ export class MineGrid extends Grid {
     // Set randoms mines at grid
     setMines( numMines ) {
         while( this.countMines() < numMines ) {
-            let x = Math.floor( Math.random() * this.col );
-            let y = Math.floor( Math.random() * this.row );
-            this.grid[ x ][ y ] = 'm';
+            let x = Math.floor( Math.random() * this.col + 1);
+            let y = Math.floor( Math.random() * this.row + 1);
+            this.grid[ x ][ y ] = new Mine();
         }
     }
 
@@ -82,88 +88,112 @@ export class MineGrid extends Grid {
         for( let i = row - 1; i <= row + 1; i++ ) {
             for( let j = col - 1; j <= col + 1; j++ ) {
                 if( i === row && j === col ) continue;
-                if( this.grid[i][j] !== 'm' )
-                    this.grid[i][j]++;
+                if( this.grid[i][j].label !== 'm' )
+                    this.grid[i][j].label++;
             }
         }
     }
 
     // Type a number according to the number of mines around for all the mines
     numerateGrid() {
-        this.addLimiters();
         for( let i = 1; i <= this.row; i++ ) {
             for( let j = 1; j <= this.col; j++ ) {
-                if( this.grid[i][j] === 'm' )
+                if( this.grid[i][j] instanceof Mine )
                     this.numerateMine(i, j);
             }
         }
-        this.removeLimiters();
-        console.table( this.grid );
-    }
-}
-
-export class ShadowGrid extends Grid {
-    constructor( mGrid = MineGrid() ) {
-        super( mGrid.row, mGrid.col );
-
-        // Props
-        this.mineGrid = mGrid;
-        this.shadowGrid;
-
-        this.hide();
+        this.showGameState();
     }
 
-    // Hide the elements of the grid
-    hide() {
-        this.shadowGrid = new Array( this.row );
-        for( let i = 0; i < this.row; i++ )
-            this.shadowGrid[i] = new Array( this.col ).fill(false, 0);
-    }
-
-    unhide( row, col ) {
-        if( this.mineGrid.grid[row][col] === 'm' ) {
-            this.shadowGrid[row][col] = 'm';
-            this.showGrid();
-            return false;
-        }
-
-        else if( Number.isInteger( this.mineGrid.grid[row][col] ) && this.mineGrid.grid[row][col] !== 0 ) {
-            this.shadowGrid[row][col] = this.mineGrid.grid[row][col];
-            this.showGrid();
-            return true;
-        }
-
-        else if ( this.mineGrid.grid[row][col] === 0 ) {
-            // FIXME: No desoculta las minas bien
-            this.mineGrid.addLimiters();
-            this.addLimiters();
-            
-            for( let i = row - 1; i <= row + 1; i++ ) {
-                for( let j = col - 1; j <= col + 1; j++ ) {
-                    if( this.mineGrid.grid[i][j] === 0 )
-                        this.shadowGrid[i][j] = " ";
-                    else
-                        this.shadowGrid[i][j] = this.mineGrid.grid[i][j];
-                }
-            }
-
-            this.mineGrid.removeLimiters();
-            this.removeLimiters();
-        }
-        
-        this.showGrid();
-        return true;
+    // State of all blocks is hide (state: false);
+    hideAll() {
+        for( let row in this.grid ) 
+            row.forEach( col => { col.state = false } );
     }
 
     unhideAll() {
-        for( let i = 0; i < this.row; i++ ) {
-            for( let j = 0; j < this.col; j++ ) {
-                this.shadowGrid[i][j] = this.mineGrid.grid[i][j];
+        for( let i = 1; i <= this.row; i++  ) {
+            for( let j = 1 ; j <= this.col; j++ ) {
+                this.grid[i][j].state = true;
             }
+        }
+        this.showGameState();
+    }
+
+    unhideBlock(row, col) {
+        if ( this.grid[row][col].label === 0 ) 
+            return this.unhideSection(row, col)
+        else
+            return this.unhideThis(row, col);
+    }
+
+    unhideThis( row, col ) {
+        if( this.grid[row][col] instanceof Mine ) {
+            this.grid[row][col].state = true;
+            return false;
+        }
+
+        if( this.grid[row][col].label !== 'm' && this.grid[row][col].label !== 0 ) {
+            this.grid[row][col].state = true;
+            return true;
         }
     }
 
-    showGrid() {
-        console.table( this.shadowGrid );
+    unhideSection( row, col) {
+            
+            for( let i = row - 1; i <= row + 1; i++ ) {
+                for( let j = col - 1; j <= col + 1; j++ ) {
+                    if( this.grid[i][j].flag ) continue;
+                    this.grid[i][j].state = true;
+                    // FIXME: Implement Recursion
+                    // this.unhideSection(i, j)
+                }
+            }
+
+            return true;
+    }
+
+    renderGame() {
+        const gameState = this.grid.slice(1, this.row + 1).map( value => value.slice(1, this.col + 1));
+        const shadowGrid = new Array();
+        
+        for( let i = 0; i < this.row; i++ ) {
+            const row = [];
+            for( let j = 0; j < this.col; j++ ) {
+                if( gameState[i][j].state )
+                    row.push( gameState[i][j].label );
+                else
+                    row.push( " - ");
+            }
+
+            shadowGrid[i] = [ ...row ];
+        }
+
+        console.table( shadowGrid );
+    }
+
+    checkWin() {
+        let count = 0;
+        for( let i = 1; i <= this.row; i++ ) {
+            for( let j = 1; j <= this.col + 1; j++ ) {
+                if( this.grid[i][j].flag && this.grid[i][j] instanceof Mine )
+                    count++;
+            }
+        }
+
+        if( count === this.mines )
+            return true;
+        else
+            return false;
+    }
+
+    checkLose() {
+        for( let i = 1; i <= this.row; i++ ) {
+            for( let j = 1; j <= this.col + 1; j++ ) {
+                if( this.grid[i][j].state && this.grid[i][j] instanceof Mine )
+                    return true;
+            }
+        }
+        return false;
     }
 }
--- src/ui.js
+++ src/ui.js
@@ -1,86 +1,133 @@
-import { ShadowGrid, MineGrid } from "./app.js";
+import  { Mine, MineGrid } from "./app.js";
 
 class UI {
-
     // FIXME: Arreglar la vista del CSS
-    static buildGrid( _shadowGrid = new ShadowGrid() ) {
+
+    // Build the Grid at UI with mines inside and labels
+    static buildGrid( _mineGrid = new MineGrid() ) {
         const mainGrid = document.getElementById('main-grid');
-        for( let i = 0; i < _shadowGrid.row; i++ ) {
+        for( let i = 1; i <= _mineGrid.row; i++ ) {
             let row = document.createElement( 'tr' );
 
-            for( let j = 0; j < _shadowGrid.col; j++ ) {
+            for( let j = 1; j <= _mineGrid.col; j++ ) {
                 let col = document.createElement( 'td' );
+
+                let number = document.createElement('span');
+                number.innerText = _mineGrid.grid[i][j].label;
+                number.setAttribute("class", "m-2");
+                number.setAttribute("id", `s-${i - 1}-${j - 1}`);
+                number.style.position = "absolute";
+                col.appendChild(number);
 
                 let button = document.createElement('button');
                 button.setAttribute("class", "btn btn-dark container");
-                button.style = `width: 30px, height: 30px`;
-                button.setAttribute("id", `${i}-${j}`);     
+                button.setAttribute("id", `b-${i - 1}-${j - 1}`);
                 col.appendChild( button );
-
-                let number = document.createElement('span');
-                number.innerText = _shadowGrid.mineGrid.grid[i][j];
-                number.setAttribute("class", "m-2")
-                button.appendChild(number);
+                //button.style = "position: relative";
 
                 row.appendChild( col );
             }
             mainGrid.appendChild( row );
         }
     }
+
+    // Update the UI Grid
+    static updateGrid( _mineGrid = new MineGrid() ) {
+        for( let i = 1; i <= _mineGrid.row; i++ ) {
+            for( let j = 1; j <= _mineGrid.col; j++ ) {
+                if( _mineGrid.grid[i][j].state ) {
+                    document.getElementById( `b-${ i - 1 }-${ j - 1 }` ).style.display = "none";
+                }
+            }
+        }
+    }
+
+    static showMessageYouLose() {
+        const mainGrid = document.getElementById('main-grid');
+        const youLose = document.createElement('div');
+        youLose.setAttribute('class', 'alert alert-danger');
+        youLose.innerText = "!!! HAS PERDIDO";
+        mainGrid.appendChild( youLose );
+    }
 }
 
 document.oncontextmenu = () => { return false; }
 
-// ---------------------- TESTING APP ------------------------ //
-
+// APP MAIN
 const g = new MineGrid(10, 10);
 g.setMines(10);
 g.numerateGrid();
-
-const s = new ShadowGrid( g );
-s.showGrid();
-s.unhide(3, 3);
-s.unhideAll();
-s.showGrid();
-
-const array = [
-    [ {obj: "mine", number: 3}, {obj: false, number: 2}, {obj: false, number: 3} ],
-    [ {obj: "mine", number: 3}, {obj: false, number: 2}, {obj: false, number: 3} ],
-    [ {obj: "mine", number: 3}, {obj: false, number: 2}, {obj: false, number: 3} ],
-    [ {obj: "mine", number: 3}, {obj: false, number: 2}, {obj: false, number: 3} ],
-]
-console.table(array);
-
-// UI.buildGrid( s );
+UI.buildGrid( g );
 
 // ---------------------- TESTING APP ------------------------ //
 
+// ---------------------- END TESTING APP ------------------------ //
+
 // Events
+
+// Click in a Grid Button
 document.querySelectorAll('td button').forEach( ( buttons ) => {
+
+    // Right Click put a Flag in the Button
+    buttons.addEventListener( 'mousedown', ( e ) => {
+        // FIXME: Investigar sobre la propagacion de eventos
+        if( e.button === 2) {
+            
+            if( e.target.localName === "button" ) {
+                const coordinates = e.target.id.split("-");
+                if( g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag ) {
+                    e.target.innerHTML = "";
+                    g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag = false;
+                }
+                else {
+                    e.target.innerHTML = `<i class="fa fa-flag style="color: white" ></i>`;
+                    g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag = true;
+                }
+            }
+            else {
+                const coordinates = e.target.parentNode.id.split("-");
+                if( g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag ) {
+                    e.target.parentNode.innerHTML = "";
+                    g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag = false;
+                }
+                else {
+                    e.target.parentNode.innerHTML = `<i class="fa fa-flag style="color: white" ></i>`;
+                    g.grid[ parseInt(coordinates[1]) + 1 ][ parseInt(coordinates[2]) + 1 ].flag = true;
+                }
+            }
+        }
+    })
+
+    // Click
+    buttons.addEventListener( 'click', ( e ) => {
+        const coordinates = e.target.id.split("-");
+        if( g.unhideBlock( parseInt(coordinates[1]) + 1, parseInt(coordinates[2]) + 1 ) )
+            UI.updateGrid( g );
+        else {
+            UI.showMessageYouLose();
+            UI.updateGrid( g );
+        }
+    })
+
+})
+
+document.querySelectorAll('td').forEach( ( blocks ) => {
 
     // FIXME: Arreglar el proceso inverso de poner la banderita (quitar la banderita)
 
-    buttons.addEventListener( 'mousedown', ( e ) => {
-        if( e.button === 2) {
-            if( e.target.localName === "span" ) {
-                e.target.innerText = "";
-                e.target.innerHTML = `<i class="fa fa-flag style="color: white" ></i>`;
-            }
-
-            if( e.target.localName === "button" ) {
-                if( e.target.children[0].localName === "i" ) {
-                    e.target.removeChild( document.querySelector('i') );
-                }
-                else {
-                    let span = e.target.children[0];
-                    span.innerText = "";
-                    span.innerHTML = `<i class="fa fa-flag style="color: white" ></i>`;
-                }
-            }
-
-            console.log(e.target);
-            console.log(e.target.children); 
-        }
+    blocks.addEventListener( 'dblclick' , ( e ) => {
+        console.log( e.target );
+        let coordinates;
+        e.target.localName === 'span' ?  coordinates = e.target.id.split("-") : coordinates = e.target.children[0].id.split("-");
+        console.log( "x: ", coordinates[1], "y: ", coordinates[2] );
+        g.unhideSection( parseInt(coordinates[1]) + 1, parseInt(coordinates[2]) + 1 );
+        UI.updateGrid( g );
+        if( g.checkLose() )
+            UI.showMessageYouLose();
     })
-    
+
+
+
 })
+
+
